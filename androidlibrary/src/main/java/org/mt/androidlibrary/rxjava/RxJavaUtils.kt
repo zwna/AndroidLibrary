358a79449efc458703ebd2189762fc96e0aac7ca
zwna <3123848646@qq.com>
2019-05-15 17:53:45 +0800

    2019-5-15 第二次提交

--- androidlibrary/src/main/java/org/mt/androidlibrary/rxjava/RxJavaUtils.kt
+++ androidlibrary/src/main/java/org/mt/androidlibrary/rxjava/RxJavaUtils.kt
@@ -17,123 +17,127 @@ import org.mt.androidlibrary.rxjava.callback.RxJavaCallBack
  */
 class RxJavaUtils {
 
-    /**
-     * 在AppCompatActivity中使用RxJava进行网络请求
-     */
-    fun <T> useInAppCompatActivity(
-        observable: Observable<T>,
-        rxAppCompatActivity: RxAppCompatActivity,
-        baseCallBack: RxJavaCallBack<T>
-    ) {
-        observable.compose(rxAppCompatActivity.bindToLifecycle())
-            .observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io()).subscribe(baseCallBack)
+    companion object{
+        /**
+         * 在AppCompatActivity中使用RxJava进行网络请求
+         */
+        fun <T> useInAppCompatActivity(
+            observable: Observable<T>,
+            rxAppCompatActivity: RxAppCompatActivity,
+            baseCallBack: RxJavaCallBack<T>
+        ) {
+            observable.compose(rxAppCompatActivity.bindToLifecycle())
+                .observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io()).subscribe(baseCallBack)
+        }
+
+        /**
+         * 在Activity中使用RxJava进行网络请求
+         */
+        fun <T> useInActivity(
+            observable: Observable<T>,
+            rxActivity: RxActivity,
+            baseCallBack: RxJavaCallBack<T>
+        ) {
+            observable.compose(rxActivity.bindToLifecycle())
+                .observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io()).subscribe(baseCallBack)
+        }
+
+        /**
+         * 在V4包下的Fragment中使用RxJava进行网络请求
+         */
+        fun <T> useInV4Fragment(
+            observable: Observable<T>,
+            rxFragment: RxFragment,
+            baseCallBack: RxJavaCallBack<T>
+        ) {
+            observable.compose(rxFragment.bindToLifecycle())
+                .observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io()).subscribe(baseCallBack)
+        }
+
+        /**
+         * 在app包下的Fragment中使用RxJava进行网络请求
+         */
+        fun <T> useInFragment(
+            observable: Observable<T>,
+            rxFragment: com.trello.rxlifecycle3.components.RxFragment,
+            baseCallBack: RxJavaCallBack<T>
+        ) {
+            observable.compose(rxFragment.bindToLifecycle())
+                .observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io()).subscribe(baseCallBack)
+        }
+
+        /**
+         * 在AppCompatActivity中使用RxJava，当activity销毁的时候解除订阅
+         */
+        fun <T> useInAppCompatActivityOnDestory(
+            observable: Observable<T>,
+            rxAppCompatActivity: RxAppCompatActivity,
+            baseCallBack: RxJavaCallBack<T>
+        ) {
+            observable.compose(rxAppCompatActivity.bindUntilEvent(ActivityEvent.DESTROY))
+                .observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io()).subscribe(baseCallBack)
+        }
+
+        /**
+         * 在Activity中使用RxJava，当activity销毁的时候解除订阅
+         */
+        fun <T> useInActivityOnDestory(
+            observable: Observable<T>,
+            rxActivity: RxActivity,
+            baseCallBack: RxJavaCallBack<T>
+        ) {
+            observable.compose(rxActivity.bindUntilEvent(ActivityEvent.DESTROY))
+                .observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io()).subscribe(baseCallBack)
+        }
+
+        /**
+         * 在V4包下的Fragment中使用RxJava进行网络请求，当Fragment销毁的时候解除订阅
+         */
+        fun <T> useInV4FragmentOnDestory(
+            observable: Observable<T>,
+            rxFragment: RxFragment,
+            baseCallBack: RxJavaCallBack<T>
+        ) {
+            observable.compose(rxFragment.bindUntilEvent(FragmentEvent.DESTROY))
+                .observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io()).subscribe(baseCallBack)
+        }
+
+        /**
+         * 在V4包下的Fragment中使用RxJava进行网络请求，当Fragment销毁视图的时候解除订阅
+         */
+        fun <T> useInV4FragmentOnDestoryView(
+            observable: Observable<T>,
+            rxFragment: RxFragment,
+            baseCallBack: RxJavaCallBack<T>
+        ) {
+            observable.compose(rxFragment.bindUntilEvent(FragmentEvent.DESTROY_VIEW))
+                .observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io()).subscribe(baseCallBack)
+        }
+
+        /**
+         * 在app包下的Fragment中使用RxJava进行网络请求，当Fragment销毁的时候解除订阅
+         */
+        fun <T> useInFragmentOnDestory(
+            observable: Observable<T>,
+            rxFragment: com.trello.rxlifecycle3.components.RxFragment,
+            baseCallBack: RxJavaCallBack<T>
+        ) {
+            observable.compose(rxFragment.bindUntilEvent(FragmentEvent.DESTROY))
+                .observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io()).subscribe(baseCallBack)
+        }
+
+        /**
+         * 在app包下的Fragment中使用RxJava进行网络请求，当Fragment销毁视图的时候解除订阅
+         */
+        fun <T> useInFragmentOnDestoryView(
+            observable: Observable<T>,
+            rxFragment: com.trello.rxlifecycle3.components.RxFragment,
+            baseCallBack: RxJavaCallBack<T>
+        ) {
+            observable.compose(rxFragment.bindUntilEvent(FragmentEvent.DESTROY_VIEW))
+                .observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io()).subscribe(baseCallBack)
+        }
     }
 
-    /**
-     * 在Activity中使用RxJava进行网络请求
-     */
-    fun <T> useInActivity(
-        observable: Observable<T>,
-        rxActivity: RxActivity,
-        baseCallBack: RxJavaCallBack<T>
-    ) {
-        observable.compose(rxActivity.bindToLifecycle())
-            .observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io()).subscribe(baseCallBack)
-    }
 
-    /**
-     * 在V4包下的Fragment中使用RxJava进行网络请求
-     */
-    fun <T> useInV4Fragment(
-        observable: Observable<T>,
-        rxFragment: RxFragment,
-        baseCallBack: RxJavaCallBack<T>
-    ) {
-        observable.compose(rxFragment.bindToLifecycle())
-            .observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io()).subscribe(baseCallBack)
-    }
-
-    /**
-     * 在app包下的Fragment中使用RxJava进行网络请求
-     */
-    fun <T> useInFragment(
-        observable: Observable<T>,
-        rxFragment: com.trello.rxlifecycle3.components.RxFragment,
-        baseCallBack: RxJavaCallBack<T>
-    ) {
-        observable.compose(rxFragment.bindToLifecycle())
-            .observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io()).subscribe(baseCallBack)
-    }
-
-    /**
-     * 在AppCompatActivity中使用RxJava，当activity销毁的时候解除订阅
-     */
-    fun <T> useInAppCompatActivityOnDestory(
-        observable: Observable<T>,
-        rxAppCompatActivity: RxAppCompatActivity,
-        baseCallBack: RxJavaCallBack<T>
-    ) {
-        observable.compose(rxAppCompatActivity.bindUntilEvent(ActivityEvent.DESTROY))
-            .observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io()).subscribe(baseCallBack)
-    }
-
-    /**
-     * 在Activity中使用RxJava，当activity销毁的时候解除订阅
-     */
-    fun <T> useInActivityOnDestory(
-        observable: Observable<T>,
-        rxActivity: RxActivity,
-        baseCallBack: RxJavaCallBack<T>
-    ) {
-        observable.compose(rxActivity.bindUntilEvent(ActivityEvent.DESTROY))
-            .observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io()).subscribe(baseCallBack)
-    }
-
-    /**
-     * 在V4包下的Fragment中使用RxJava进行网络请求，当Fragment销毁的时候解除订阅
-     */
-    fun <T> useInV4FragmentOnDestory(
-        observable: Observable<T>,
-        rxFragment: RxFragment,
-        baseCallBack: RxJavaCallBack<T>
-    ) {
-        observable.compose(rxFragment.bindUntilEvent(FragmentEvent.DESTROY))
-            .observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io()).subscribe(baseCallBack)
-    }
-
-    /**
-     * 在V4包下的Fragment中使用RxJava进行网络请求，当Fragment销毁视图的时候解除订阅
-     */
-    fun <T> useInV4FragmentOnDestoryView(
-        observable: Observable<T>,
-        rxFragment: RxFragment,
-        baseCallBack: RxJavaCallBack<T>
-    ) {
-        observable.compose(rxFragment.bindUntilEvent(FragmentEvent.DESTROY_VIEW))
-            .observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io()).subscribe(baseCallBack)
-    }
-
-    /**
-     * 在app包下的Fragment中使用RxJava进行网络请求，当Fragment销毁的时候解除订阅
-     */
-    fun <T> useInFragmentOnDestory(
-        observable: Observable<T>,
-        rxFragment: com.trello.rxlifecycle3.components.RxFragment,
-        baseCallBack: RxJavaCallBack<T>
-    ) {
-        observable.compose(rxFragment.bindUntilEvent(FragmentEvent.DESTROY))
-            .observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io()).subscribe(baseCallBack)
-    }
-
-    /**
-     * 在app包下的Fragment中使用RxJava进行网络请求，当Fragment销毁视图的时候解除订阅
-     */
-    fun <T> useInFragmentOnDestoryView(
-        observable: Observable<T>,
-        rxFragment: com.trello.rxlifecycle3.components.RxFragment,
-        baseCallBack: RxJavaCallBack<T>
-    ) {
-        observable.compose(rxFragment.bindUntilEvent(FragmentEvent.DESTROY_VIEW))
-            .observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io()).subscribe(baseCallBack)
-    }
 }
