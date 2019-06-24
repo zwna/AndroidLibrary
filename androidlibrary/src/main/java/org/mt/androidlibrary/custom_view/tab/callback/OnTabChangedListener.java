b42f84cff1e9acc0154628613789e76619678975
zwna <3123848646@qq.com>
2019-06-24 17:48:43 +0800

    新增仿微信底部导航栏 升级第三方包版本

---
+++ androidlibrary/src/main/java/org/mt/androidlibrary/custom_view/tab/callback/OnTabChangedListener.java
@@ -0,0 +1,10 @@
+package org.mt.androidlibrary.custom_view.tab.callback;
+
+/**
+ *@Description:AlphaTabsIndicator类底部Tab被选中的监听事件
+ *@Author:zwna
+ *@Date:2019/4/15
+ */
+public interface OnTabChangedListener {
+    void onTabSelected(int tabNum);
+}
